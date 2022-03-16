b2781abb8fdf681f92731f598e4a08ffb33431a0
Denis Rosset <physics@denisrosset.com>
2022-03-16 10:53:18 +0100

    feat: reexport configpile.Result

--- src/configpile/__init__.py
+++ src/configpile/__init__.py
@@ -2,5 +2,5 @@ __version__ = "7.1.3"
 
 from .arg import AutoName, Expander, Param, Positional
 from .config import Config
-from .errors import Err
+from .errors import Err, Result
 from .types import ParamType
