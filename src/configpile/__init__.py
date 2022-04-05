b61be667e6577c1ec6b6aa0dea815eb44bb1975d
Denis Rosset <physics@denisrosset.com>
2022-04-05 10:37:47 +0200

    feat: export ForceCase from the main configpile package

--- src/configpile/__init__.py
+++ src/configpile/__init__.py
@@ -3,7 +3,7 @@ __version__ = "8.0.0"
 from . import parsers
 from .arg import Derived, Expander, Param, Positional
 from .config import Config
-from .parsers import Parser
+from .parsers import ForceCase, Parser
 from .userr import Err, Res
 
 __all__ = [
@@ -16,4 +16,5 @@ __all__ = [
     "Parser",
     "Positional",
     "parsers",
+    "ForceCase",
 ]
